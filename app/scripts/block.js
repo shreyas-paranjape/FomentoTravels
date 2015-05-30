e0c36afb9de5d6fbccc576d96e040b48017cc56e
Shreyas Paranjape <sparanjape89@gmail.com>
2015-05-30 09:16:53 +0530

    made style changes

--- app/scripts/block.js
+++ app/scripts/block.js
@@ -1,7 +1,7 @@
 /*global $:false */
 $(document).ready(function () {
     'use strict';
-    var fader = function (){
+    var fader = function () {
         var that = $(this),
             clone = that.clone(),
             offset = that.offset(),
@@ -9,42 +9,42 @@ $(document).ready(function () {
         $('.block').unbind('click');
         clone
             .addClass('clone')
-            .css({'top':offset.top,'left':offset.left})
-            .bind('click',function () {})
+            .css({'top': offset.top, 'left': offset.left})
+            .bind('click', function () {})
             .prependTo(blocks)
-            .animate({ top:100, left:100, width : blocks.width() },1000,'swing',function (){
-                    $('.fadout',clone).toggle();
-                    $('.content',clone).toggle();
-                    $('.content-more',clone).toggle();
+            .animate({ top: 100, left: 100, width : blocks.width() }, 1000, 'swing', function () {
+                $('.fadout', clone).toggle();
+                $('.content', clone).toggle();
+                $('.content-more', clone).toggle();
             });
-        
         $('.block').not(clone)
-            .animate({opacity:0},1000,'swing',function (){});
-        
-        $('.fadout',clone).bind('click', function (){
-            var topCurrent,leftCurrent;
-            if(clone.hasClass('one')){
+            .animate({opacity: 0}, 1000, 'swing', function () {});
+        $('footer').animate({opacity: 0}, 1000, 'swing', function () {});
+        $('.fadout', clone).bind('click', function () {
+            var topCurrent, leftCurrent;
+            if (clone.hasClass('one')) {
                 topCurrent = offset.top;
                 leftCurrent = offset.left;
-            }else if(clone.hasClass('two')){
+            } else if (clone.hasClass('two')) {
                 topCurrent = offset.top;
                 leftCurrent = offset.left + that.width();
-            }else if(clone.hasClass('three')){
+            } else if (clone.hasClass('three')) {
                 topCurrent = offset.top + that.height();
                 leftCurrent = offset.left;
-            }else if(clone.hasClass('four')){
+            } else if (clone.hasClass('four')) {
                 topCurrent = offset.top + that.height();
                 leftCurrent = offset.left + that.width();
             }
-            $('.block').not(clone).bind('click',fader);
-            clone.animate({ top:topCurrent, left:leftCurrent , width:0, height:0},1000,'swing',function (){
-                $('.fadout',clone).toggle();
+            $('.block').not(clone).bind('click', fader);
+            clone.animate({ top: topCurrent, left: leftCurrent, width: 0, height: 0}, 1000, 'swing', function () {
+                $('.fadout', clone).toggle();
             });
-            $('.block').not(clone).animate({opacity:1},1000,'swing',function (){
+            $('.block').not(clone).animate({opacity: 1}, 1000, 'swing', function () {
                 clone.remove();
             });
+            $('footer').animate({opacity: 1}, 1000, 'swing', function () {});
         });
     };
-    $('.block').bind('click',fader);
+    $('.block').bind('click', fader);
     
 });
