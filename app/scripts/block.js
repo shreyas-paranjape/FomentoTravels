a4d7d617361a96c8615aa03407785d7cd0676ed1
Shreyas Paranjape <sparanjape89@gmail.com>
2015-07-02 14:58:24 +0530

    transition changes

--- app/scripts/block.js
+++ app/scripts/block.js
@@ -15,28 +15,36 @@ $(document).ready(function () {
                 .animate({opacity: 0}, 1000, 'swing', function () {});
         
         $('.fadout', clone).toggle();
-        $('.content', clone).remove();
-        clone.css("background-color","white");
+        /*$('.content', clone).remove();
+        clone.css("background-color","white");*/
 
         clone
             .addClass('clone')
-            .css({'top': offset.top - 4, 'left': offset.left - 4, width: that.width(), height: that.height()})
+            .css({'top': offset.top - 4, 'left': offset.left - 4, width: that.width()})
             .bind('click', function () {})
             .prependTo(blocks)
             .animate({ top: blockOneOffset.top - 4,
                       left: blockOneOffset.left - 4,
-                      width : blocks.width() + 18,
-                      height: blocks.height()}, 1000, 'swing',
+                      width : blocks.width() + 18
+                      /*height: blocks.height()*/}, 1000, 'swing',
                 function () {
-                    $('.content-more', clone).toggle();
+                     //$('.content', clone).remove();
+                    // $('.content-more', clone).toggle();
                 });
-        $('.fadout', clone).bind('click', function () {
+       
+        $('.content', clone).animate({opacity: 0},600, 'swing',function(){
+            $('.content', clone).remove();
             $('.content-more', clone).toggle();
+            $('.content-more', clone).animate({opacity: 1},500, 'swing');
+        });
+        //$('.content-more', clone).animate({opacity: 1},1000, 'swing');
+        $('.fadout', clone).bind('click', function () {
             $('.block').not(clone).bind('click', fader);
-            clone.animate({ top: offset.top - 4, left: offset.left - 4, width: that.width(), height: that.height()}, 1000);
+            clone.animate({ top: offset.top - 4, left: offset.left - 4, width: that.width(), height: that.height()}, 800);
             $('.block').not(clone).animate({opacity: 1}, 1000, 'swing', function () {
                 clone.remove();
             });
+            $('.content-more', clone).animate({opacity: 0},1000, 'swing');
         });
     };
     $('.block').bind('click', fader);
