09b70d8c917d74e837f61f5a8273e061f809b36c
Shreyas Paranjape <sparanjape89@gmail.com>
2015-07-14 10:16:28 +0530

    improved block transition, changed trans bg

--- app/scripts/block.js
+++ app/scripts/block.js
@@ -1,25 +1,17 @@
 /*global $:false */
 $(document).ready(function () {
     'use strict';
-     //$('.blocks').height($(window).height());
     var fader = function () {
         var that = $(this),
             clone = that.clone(),
             offset = that.offset(),
             blockOneOffset = $('.block').filter('.one').offset(),
             blocks = that.parent().parent().parent();
-        
         $('.block')
             .unbind('click')
             .not(clone)
-                .animate({opacity: 0}, 1000, 'swing', function () {});
-        
+                .animate({opacity: 0}, 0, 'swing', function () {});
         $('footer').toggle();
-        
-        $('.fadout', clone).toggle();
-        /*$('.content', clone).remove();
-        clone.css("background-color","white");*/
-
         clone
             .addClass('clone')
             .css({'top': offset.top - 4, 'left': offset.left - 4, width: that.width()})
@@ -28,32 +20,26 @@ $(document).ready(function () {
             .animate({ top: blockOneOffset.top - 4,
                       left: blockOneOffset.left - 4,
                       width : blocks.width() + 18
-                      /*height: blocks.height()*/}, 1000, 'swing',
+                      }, 1000, 'swing',
                 function () {
-                     //$('.content', clone).remove();
-                    // $('.content-more', clone).toggle();
+                    $('.content-more', clone).toggle();
+                    $('.content-more', clone).animate({opacity: 1}, 100, 'swing');
+                    $('.content', clone).remove();
+                    $('.fadout', clone).toggle();
                 });
-       
-        $('.content', clone).animate({opacity: 0},600, 'swing',function(){
-            $('.content', clone).remove();
-            $('.content-more', clone).toggle();
-            $('.content-more', clone).animate({opacity: 1},500, 'swing');
+        $('.content', clone).animate({opacity: 0}, 600, 'swing', function () {
         });
-        //$('.content-more', clone).animate({opacity: 1},1000, 'swing');
         $('.fadout', clone).bind('click', function () {
+            $('.fadout', clone).toggle();
             $('footer').toggle();
+            $('.overlay-content', clone).toggle();
             $('.block').not(clone).bind('click', fader);
             clone.animate({ top: offset.top - 4, left: offset.left - 4, width: that.width(), height: that.height()}, 800);
             $('.block').not(clone).animate({opacity: 1}, 1000, 'swing', function () {
                 clone.remove();
             });
-            $('.content-more', clone).animate({opacity: 0},1000, 'swing');
+            $('.content-more', clone).animate({opacity: 0}, 1000, 'swing');
         });
     };
     $('.block').bind('click', fader);
-    
-    window.onhashchange = function(){
-        console.log(location.hash);
-    };
-    
 });
