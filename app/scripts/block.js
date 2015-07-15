74c25c80d9ac96b13b2fce64b8eeda496429b297
Ruchita <ruchitaelekar05@gmail.com>
2015-07-15 10:57:49 +0530

    almost done

--- app/scripts/block.js
+++ app/scripts/block.js
@@ -10,16 +10,16 @@ $(document).ready(function () {
         $('.block')
             .unbind('click')
             .not(clone)
-                .animate({opacity: 0}, 0, 'swing', function () {});
+                .animate({opacity: 0}, 400, 'swing', function () {});
         $('footer').animate({opacity: 0}, 0);
         clone
             .addClass('clone')
-            .css({'top': offset.top - 4, 'left': offset.left - 4, width: that.width()})
+            .css({'top': offset.top, 'left': offset.left, width: that.width()})
             .bind('click', function () {})
             .prependTo(blocks)
-            .animate({ top: blockOneOffset.top - 4,
-                      left: blockOneOffset.left - 4,
-                      width : blocks.width() + 18
+            .animate({ top: blockOneOffset.top,
+                      left: blockOneOffset.left,
+                      width : blocks.width()
                       }, 1000, 'swing',
                 function () {
                     $('.content-more', clone).toggle();
@@ -31,12 +31,14 @@ $(document).ready(function () {
         });
         $('.fadout', clone).bind('click', function () {
             $('.fadout', clone).toggle();
-             $('footer').animate({opacity: 1}, 0);
+            $('footer').animate({opacity: 1}, 0);
             $('.overlay-content', clone).toggle();
             $('.block').not(clone).bind('click', fader);
-            clone.animate({ top: offset.top - 4, left: offset.left - 4, width: that.width(), height: that.height()}, 800);
-            $('.block').not(clone).animate({opacity: 1}, 1000, 'swing', function () {
+            clone.animate({ top: offset.top, left: offset.left,
+                           width: that.width(), height: that.height()}, 800, 'swing', function () {
                 clone.remove();
+            });
+            $('.block').not(clone).animate({opacity: 1}, 1000, 'swing', function () {
             });
             $('.content-more', clone).animate({opacity: 0}, 1000, 'swing');
         });
