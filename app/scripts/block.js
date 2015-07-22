1ad8a4c8dcfda3c539f2bfc7d0535db2dbc657dd
Ruchita <ruchitaelekar05@gmail.com>
2015-07-22 10:58:29 +0530

    content.html added to the final website

--- app/scripts/block.js
+++ app/scripts/block.js
@@ -1,6 +1,7 @@
 /*global $:false */
 $(document).ready(function () {
     'use strict';
+
     var fader = function () {
         var that = $(this),
             clone = that.clone(),
@@ -10,37 +11,60 @@ $(document).ready(function () {
         $('.block')
             .unbind('click')
             .not(clone)
-                .animate({opacity: 0}, 400, 'swing', function () {});
-        $('footer').animate({opacity: 0}, 0);
+            .animate({
+                opacity: 0
+            }, 400, 'swing', function () {});
+        $('footer').animate({
+            opacity: 0
+        }, 0);
         clone
             .addClass('clone')
-            .css({'top': offset.top, 'left': offset.left, width: that.width()})
+            .css({
+                'top': offset.top,
+                'left': offset.left,
+                width: that.width()
+            })
             .bind('click', function () {})
             .prependTo(blocks)
-            .animate({ top: blockOneOffset.top,
-                      left: blockOneOffset.left,
-                      width : blocks.width()
-                      }, 1000, 'swing',
+            .animate({
+                    top: blockOneOffset.top,
+                    left: blockOneOffset.left,
+                    width: blocks.width()
+                }, 1000, 'swing',
                 function () {
-                    $('.content-more', clone).toggle();
-                    $('.content-more', clone).animate({opacity: 1}, 100, 'swing');
+                    var contentMore = $('.content-more', clone);
+                    contentMore.load(contentMore.attr('href'));
+                    contentMore.toggle();
+                    contentMore.animate({
+                        opacity: 1
+                    }, 100, 'swing');
                     $('.content', clone).remove();
                     $('.fadout', clone).toggle();
                 });
-        $('.content', clone).animate({opacity: 0}, 600, 'swing', function () {
-        });
+        $('.content', clone).animate({
+            opacity: 0
+        }, 600, 'swing', function () {});
         $('.fadout', clone).bind('click', function () {
             $('.fadout', clone).toggle();
-            $('footer').animate({opacity: 1}, 0);
+            $('footer').animate({
+                opacity: 1
+            }, 0);
             $('.overlay-content', clone).toggle();
             $('.block').not(clone).bind('click', fader);
-            clone.animate({ top: offset.top, left: offset.left,
-                           width: that.width(), height: that.height()}, 800, 'swing', function () {
+            clone.animate({
+                top: offset.top,
+                left: offset.left,
+                width: that.width(),
+                height: that.height()
+            }, 800, 'swing', function () {
                 clone.remove();
             });
-            $('.block').not(clone).animate({opacity: 1}, 1000, 'swing', function () {
-            });
-            $('.content-more', clone).animate({opacity: 0}, 1000, 'swing');
+            $('.block').not(clone).animate({
+                opacity: 1
+            }, 1000, 'swing', function () {});
+            $('.content-more', clone).animate({
+                opacity: 0
+            }, 1000, 'swing');
         });
     };
     $('.block').bind('click', fader);
